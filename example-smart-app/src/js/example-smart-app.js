e8bd608481e11131425f45bfd72fa6a682ef7cc9
MOHANRAJ H <brockmohan007@gmail.com>
2020-08-19 17:58:45 +0530

    bmi9

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -45,11 +45,11 @@
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
           var bmi =null;
-          if (getQuantityUnit(height[0]) == 'in') {
-            bmi = 80/ Math.pow(((height[0].valueQuantity.value*2.54)/100), 2);
+          if (getQuantityUnit(height[0]) == 'in' && weight != '' && height != '') {
+            bmi = height[0].valueQuantity.value/ Math.pow(((height[0].valueQuantity.value*2.54)/100), 2);
           }
-          else {
-            bmi = 80/ Math.pow(((height[0].valueQuantity.value)/100), 2);
+          else if (weight != '' && height != '') {
+            bmi = height[0].valueQuantity.value/ Math.pow(((height[0].valueQuantity.value)/100), 2);
           }
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
