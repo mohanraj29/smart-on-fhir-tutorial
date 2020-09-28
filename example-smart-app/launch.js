be10f24abb20b72cbb4c3804046a28c5d82a99c5
MOHANRAJ H <brockmohan007@gmail.com>
2020-09-28 17:24:32 +0530

    create new js,css files2

---
+++ example-smart-app/launch.js
@@ -0,0 +1,4 @@
+FHIR.oauth2.authorize({
+    'client_id': '8dec8fb2-f2d3-4ebd-ae7c-6ac98e88f7c7',
+    'scope':  'patient/Patient.read patient/Observation.read launch online_access openid profile'
+  });
